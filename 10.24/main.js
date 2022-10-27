44a5e06b1220d5ef7b7be3f7ba873699a900a653
김동현 <wjdqls149@naver.com>
2022-10-28 00:00:46 +0900

    delete file upload

--- 10.24/main.js
+++
@@ -1,9 +0,0 @@
-let box = document.querySelectorAll(".box");
-anime({
-    targets: box,
-    scale: [
-      { value: 0.1, easing: 'easeOutSine', duration: 500 },
-      { value: 1, easing: 'easeInOutQuad', duration: 1200 }
-    ],
-    delay: anime.stagger( 200, { grid: [ 14, 5 ], from: 'center' } )
-});
