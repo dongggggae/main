7a9b02e3a1038d7c89890be1517b768d1886f3e5
김동현 <wjdqls149@naver.com>
2022-10-31 23:55:01 +0900

    수정

--- Up/js/main.js
+++ Up/js/main.js
@@ -26,6 +26,178 @@
 // intro\
 
 (() => {
+  String.prototype.toKorChars = function () {
+    var cCho = [
+        "ㄱ",
+        "ㄲ",
+        "ㄴ",
+        "ㄷ",
+        "ㄸ",
+        "ㄹ",
+        "ㅁ",
+        "ㅂ",
+        "ㅃ",
+        "ㅅ",
+        "ㅆ",
+        "ㅇ",
+        "ㅈ",
+        "ㅉ",
+        "ㅊ",
+        "ㅋ",
+        "ㅌ",
+        "ㅍ",
+        "ㅎ",
+      ],
+      cJung = [
+        "ㅏ",
+        "ㅐ",
+        "ㅑ",
+        "ㅒ",
+        "ㅓ",
+        "ㅔ",
+        "ㅕ",
+        "ㅖ",
+        "ㅗ",
+        "ㅘ",
+        "ㅙ",
+        "ㅚ",
+        "ㅛ",
+        "ㅜ",
+        "ㅝ",
+        "ㅞ",
+        "ㅟ",
+        "ㅠ",
+        "ㅡ",
+        "ㅢ",
+        "ㅣ",
+      ],
+      cJong = [
+        "",
+        "ㄱ",
+        "ㄲ",
+        "ㄳ",
+        "ㄴ",
+        "ㄵ",
+        "ㄶ",
+        "ㄷ",
+        "ㄹ",
+        "ㄺ",
+        "ㄻ",
+        "ㄼ",
+        "ㄽ",
+        "ㄾ",
+        "ㄿ",
+        "ㅀ",
+        "ㅁ",
+        "ㅂ",
+        "ㅄ",
+        "ㅅ",
+        "ㅆ",
+        "ㅇ",
+        "ㅈ",
+        "ㅊ",
+        "ㅋ",
+        "ㅌ",
+        "ㅍ",
+        "ㅎ",
+      ],
+      cho,
+      jung,
+      jong;
+    var str = this,
+      cnt = str.length,
+      chars = [],
+      cCode;
+    for (var i = 0; i < cnt; i++) {
+      cCode = str.charCodeAt(i);
+      if (cCode == 32) {
+        chars.push(" ");
+        continue;
+      } // 한글이 아닌 경우
+      if (cCode < 0xac00 || cCode > 0xd7a3) {
+        chars.push(str.charAt(i));
+        continue;
+      }
+      cCode = str.charCodeAt(i) - 0xac00;
+
+      jong = cCode % 28;
+      // 종성
+      jung = ((cCode - jong) / 28) % 21;
+
+      // 중성
+      cho = ((cCode - jong) / 28 - jung) / 21;
+      // 초성
+
+      //기본 코드 테스트가 ㅌㅔㅅ-ㅌ- 형식으로 저장됨
+      // chars.push(cCho[cho], cJung[jung]);
+      // if (cJong[jong] !== '') {
+      //     chars.push(cJong[jong]);
+      //     }
+
+      //  테스트라는 문장이 있으면 ㅌ테ㅅ스ㅌ트 형식으로 저장되도록함 (타이핑을 위해서)
+      chars.push(cCho[cho]);
+      chars.push(String.fromCharCode(44032 + cho * 588 + jung * 28));
+      if (cJong[jong] !== "") {
+        chars.push(String.fromCharCode(44032 + cho * 588 + jung * 28 + jong));
+      }
+    }
+
+    return chars;
+  };
+
+  //타이핑할 문장
+  var result = "함께+성장+지금+여기+";
+  var typeing1 = [];
+  result = result.split(""); // 한글자씩자름
+
+  //각글자 초성,중성,종성으로 나눔
+  for (var i = 0; i < result.length; i++) {
+    typeing1[i] = result[i].toKorChars();
+  }
+  console.log(typeing1);
+
+  //출력할 엘리먼트요소 가져옴
+  var resultDiv = document.getElementById("intro_txt");
+
+  //
+  var text = "";
+  var i = 0;
+  var j = 0;
+  var text = "";
+
+  //총글자수
+  var imax = typeing1.length;
+
+  //setInterval을 이용해 반복적으로 출력
+  var inter = setInterval(typi, 60);
+
+  function rp() {
+    let str = document.getElementById("intro_txt").innerText;
+    str1 = str
+      .replace("함께+", "함께+<i>")
+      .replace("성장+", "성장+</i>")
+      .replace("지금+", "지금+<i>")
+      .replace("여기+", "여기+</i>");
+    document.getElementById("intro_txt").innerHTML = str1;
+  }
+
+  function typi() {
+    //글자수만큼 반복후 종료
+    if (i <= imax - 1) {
+      //각 글자가 초성 중성 종성 순서대로 추가되도록
+      var jmax = typeing1[i].length;
+      resultDiv.innerHTML = text + typeing1[i][j];
+      j++;
+      if (j == jmax) {
+        text += typeing1[i][j - 1]; //초성중성종성 순서대로 출력된 글자는 저장 ( 다음 글자와 이어붙이기 위해서 )
+        i++;
+        j = 0;
+      }
+      rp();
+    } else {
+      clearInterval(inter);
+    }
+  }
   var intro_mo = document.getElementById("intro");
   var header = document.getElementById("header");
   let firstMain = document.getElementById("tab1");
@@ -103,7 +275,6 @@
     slideBgs = document.querySelectorAll(".bg"),
     bgWrap = document.querySelector(".bgWrap"),
     bgCount = slideBgs.length,
-    textCount = slideTexts.length,
     currentIndex = 0,
     number,
     wheelTimer;
@@ -130,11 +301,30 @@
   });
 
   slideTexts.forEach((text) => {
-    text.addEventListener("click", (e) => {});
+    text.addEventListener("click", (e) => {
+      e.preventDefault();
+      let index = e.currentTarget.dataset.index * 1 - 1;
+      activeTitle();
+      activeBg();
+      if (index < number) {
+        currentIndex--;
+        moveSLide(currentIndex);
+        activeTitle();
+        activeBg();
+      } else if (index > number) {
+        currentIndex++;
+        moveSLide(currentIndex);
+        activeTitle();
+        activeBg();
+      }
+      console.log(number, "num");
+      console.log(index, "in");
+      console.log(currentIndex, "cur");
+    });
   });
 
   function goToBack() {
-    if (currentIndex == 11) {
+    if (currentIndex == 13) {
       bgWrap.classList.remove("animate");
       textWrap.classList.remove("animate");
       currentIndex = 0;
@@ -142,7 +332,7 @@
         bgWrap.classList.add("animate");
         textWrap.classList.add("animate");
       }, 150);
-    } else if (currentIndex == -11) {
+    } else if (currentIndex == -13) {
       bgWrap.classList.remove("animate");
       textWrap.classList.remove("animate");
       currentIndex = 0;
@@ -154,32 +344,31 @@
   }
 
   function activeTitle() {
-    number = currentIndex + 10;
+    number = currentIndex + 12;
     slideTexts.forEach((text, number) => {
       text.classList.remove("on");
       text.classList.remove("sub");
     });
-    if (number < 0 || number > 20) {
+    if (number < 0 || number > 24) {
       currentIndex = 0;
     } else {
       slideTexts[number].classList.add("on");
     }
     if (number == 0) {
       slideTexts[number + 1].classList.add("sub");
-    } else if (number == 20) {
+    } else if (number == 24) {
       slideTexts[number - 1].classList.add("sub");
     } else {
       slideTexts[number - 1].classList.add("sub");
       slideTexts[number + 1].classList.add("sub");
     }
-    console.log(number);
   }
 
   // number +10 한 이유는 21%2 10.5 인데 인덱스는 0부터 시작하니까 중간위치 10번을 햔재 번호로 지정하기 위해
   // 2n 개 추가하면 number 은 current + 10+n
 
   function activeBg() {
-    number = currentIndex + 10;
+    number = currentIndex + 12;
     slideBgs.forEach((bg, number) => {
       bg.classList.remove("on");
       bg.classList.remove("sub");
@@ -187,7 +376,7 @@
     slideBgs[number].classList.add("on");
     if (number == 0) {
       slideBgs[number + 1].classList.add("sub");
-    } else if (number == 20) {
+    } else if (number == 24) {
       slideBgs[number - 1].classList.add("sub");
     } else {
       slideBgs[number - 1].classList.add("sub");
@@ -206,10 +395,10 @@
     slideBgs[i].style.left = i * 100 + 50 + "%";
   }
   // jquery
-  slideTexts[9].classList.add("sub");
   slideTexts[11].classList.add("sub");
-  slideTexts[10].classList.add("on");
-  slideBgs[10].classList.add("on");
+  slideTexts[13].classList.add("sub");
+  slideTexts[12].classList.add("on");
+  slideBgs[12].classList.add("on");
 })();
 
 // tab3
